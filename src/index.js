6f75300d8b4c4eeb525bec1c600d4ad3c165706f
Rafael Petrachini <raffp@me.com>
2022-10-13 10:19:34 -0700

    Add all files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
-// User Interface Logic 
-import { Constructor, Constructor2 } from './constructor.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import DinoGenerator from './project.js';
 
-const constructor = new Constructor(3, 3, 3);
+// UI Logic
+
+const dinoGenerator = new DinoGenerator();
+
+const button = document.getElementById('button');
+button.addEventListener("click", dinoGenerator.getDino);
+
+
+
+
+
+
+
+
+
+
+
+
+// function getDino() {
+//   let promise = DinoIpsum.getDino();
+//   promise.then(function(dinoDataArray) {
+//     printElements(dinoDataArray);
+//   },function(errorArray) {
+//     printError(errorArray);
+//   });
+// }
+
+// // User Interface Logic 
+// function printElements() {
+//   document.getElementById('result').innerText = this.response;
+// }
+
+// function printError(error) {
+//   document.getElementById('result').innerText = `There was an error accessing the dino of the day ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
+// }
+
+// function handleFormSubmission(event) {
+//   event.preventDefault();
+//   const dinoResult = new DinoIpsum();
+//   document.getElementById('result').innerHTML = dinoResult;
+// }
--- src/project.js
+++ src/project.js
@@ -1,1 +1,65 @@
-// Buisiness Logic
+// BL
+
+export default class DinoGenerator {
+  getDino() {
+    let request = new XMLHttpRequest();
+    const url = 'https://dinoipsum.com/api/?format=json&paragraphs=1&words=1';
+    request.addEventListener("loadend", function() {
+      if (this.status === 200) {
+        document.getElementById('result').innerHTML = "The first letter is: " + this.response;
+      } else {
+        return 'statement';
+      }
+    });
+    request.open('GET', url, true);
+    request.send();
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default class DinoIpsum {
+//   static getDino() {
+//     return new Promise(function(resolve, reject) {
+//       let request = new XMLHttpRequest(); 
+//       const url = 'https://dinoipsum.com/api/?format=json&paragraphs=1&words=1';
+//       request.addEventListener("loadend", function() {
+//         const response = JSON.parse(this.responseText);
+//         console.log(response);
+//         if (this.status === 200) {
+//           resolve([response]);
+//         } else {
+//           reject([this, response]);
+//         }
+//       });
+//       request.open("GET", url, true);
+//       request.send();
+//     });
+//   }
+
+//   userInput() {
+//     if (userDino === this.dinoName){
+//       return "positive statement"
+//     } else {
+//       return "negitive statement"
+//     }
+//   }
+// }
+  
+    
